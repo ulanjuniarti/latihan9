66d0f398275fd970c86ed782ab0bdc5ea3a2709e
ulanjuniarti <ulanjuniarti82@gmail.com>
2023-11-28 14:41:13 +0700

    menambahkan detail-detail dan mempercantik tampilan

--- lib/home.dart
+++ lib/home.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lunar Beauty'),
-        backgroundColor: Color(0xFFE2E2E2), // Set background color for AppBar
+        backgroundColor: Color(0xFFE2E2E2),
         actions: [
           TextButton(
             onPressed: () {
@@ -26,7 +26,7 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      backgroundColor: Color(0xFFE2E2E2), // Set background color for the entire page
+      backgroundColor: Color(0xFFE2E2E2),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -50,7 +50,7 @@ class HomePage extends StatelessWidget {
                           'Welcome To Lunar Beauty Tutorials',
                           style: TextStyle(
                             fontSize: 16.0,
-                            color: Colors.white, // Adjust text color based on the image content
+                            color: Colors.white,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -59,9 +59,7 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-
               SizedBox(height: 16.0),
-
               Text(
                 'Your Beauty Journey Starts Here',
                 style: TextStyle(
@@ -69,45 +67,33 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-
               SizedBox(height: 16.0),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Expanded(
-                    child: _buildCard('Make up'),
+                    child: _buildCard('Make up', 'https://i.pinimg.com/564x/47/55/c9/4755c98c5d3ffa23c099f89b0ff038dc.jpg'),
                   ),
                   Expanded(
-                    child: _buildCard('Skincare'),
+                    child: _buildCard('Skincare', 'https://i.pinimg.com/564x/18/f3/7f/18f37f7f0f5b301ca6af0bbe5ab67175.jpg'),
+                  ),
+                  Expanded(
+                    child: _buildCard('Body', 'https://i.pinimg.com/564x/4c/9f/28/4c9f28244edc0eb4508b48505d6d6ebb.jpg'),
                   ),
                 ],
               ),
-
               SizedBox(height: 16.0),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Expanded(
-                    child: _buildCard('Body'),
+                    child: _buildCard('Hair', 'https://i.pinimg.com/564x/c0/09/08/c00908e03c866ede8ce174eca9f29dab.jpg'),
                   ),
                   Expanded(
-                    child: _buildCard('Hair'),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 16.0),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                    child: _buildCard('Nails'),
+                    child: _buildCard('Nails', 'https://i.pinimg.com/564x/c0/d8/af/c0d8afa153ecedb877964df87e4c0afc.jpg'),
                   ),
                   Expanded(
-                    child: _buildCard('Brands'),
+                    child: _buildCard('Brands', 'https://i.pinimg.com/564x/6c/93/9a/6c939a0e7de4c28b44eb35eec7eb83ec.jpg'),
                   ),
                 ],
               ),
@@ -118,24 +104,32 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _buildCard(String title) {
-    return Card(
-      elevation: 3.0,
-      color: Colors.blueGrey[100], // Set soft pink color for the Card
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
+  Widget _buildCard(String title, String imageUrl) {
+    return Column(
+      children: [
+        Card(
+          elevation: 3.0,
+          color: Colors.blueGrey[100],
+          child: Container(
+            height: 80.0,
+            width: 80.0,
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
             ),
-          ],
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/nailart.dart
+++ lib/nailart.dart
@@ -5,7 +5,8 @@ class NailArtPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tips Membuat Nial Art'),
+        backgroundColor: Color(0xFFE2E2E2),
+        title: Text('Lunar Beauty'),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -14,100 +15,132 @@ class NailArtPage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // Bagian Foto
-              Image.network(
-                'https://mmc.tirto.id/image/2023/04/11/matte-nails_ratio-16x9.jpg',
-                height: 200, // Sesuaikan dengan kebutuhan Anda
-                fit: BoxFit.cover,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Anti Gagal, Tips Mudah Membuat Nail Art Sendiri di Rumah',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Image.network(
+                    'https://mmc.tirto.id/image/2023/04/11/matte-nails_ratio-16x9.jpg',
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      Text(
+                        'Gea Aletha -',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 4.0),
+                      Text(
+                        '2 days ago',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                ],
               ),
-              SizedBox(height: 16.0), // Beri sedikit jarak antara gambar dan teks
 
               // Subjudul yang bold
               Text(
                 'Tentukan Tema Nail Art',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 8.0), // Beri sedikit jarak antara subjudul dan teks
+              SizedBox(height: 8.0),
 
               // Teks
               Text(
                 'Menentukan tema nail art yang akan digunakan adalah langkah pertama yang perlu kamu lakukan. Di tahun 2020 ini tren nail art memiliki beberapa tema, seperti gradasi, ombre, hingga rasi bintang. Manfaatkanlah media Instagram untuk mencari tema-tema nail art yang booming dan kamu sukai.',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 14.0,
                 ),
               ),
-              SizedBox(height: 16.0), // Tambahkan jarak antara teks sebelumnya dan setelahnya
+              SizedBox(height: 16.0),
 
               Text(
                 'Bersihkan Kuku',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 8.0), // Beri sedikit jarak antara subjudul dan teks
+              SizedBox(height: 8.0),
 
               // Teks
               Text(
                 'Nah, langkah selanjutnya yang harus kamu lakukan adalah membersihkan kuku. Jika masih ada sisa-sisa kuteks, gunakanlah nail polish remover agar benar-benar bersih. Selain itu, kamu juga perlu membersihkan kutikula dan merapikan kuku menggunakan gunting. Kemudian bentuk kuku sesuai dengan selera agar hasil nail art nanti lebih cantik.',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 14.0,
                 ),
               ),
-              SizedBox(height: 16.0), // Tambahkan jarak antara teks sebelumnya dan setelahnya
+              SizedBox(height: 16.0),
 
               Text(
                 'Aplikasikan Base Coat',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 8.0), // Beri sedikit jarak antara subjudul dan teks
+              SizedBox(height: 8.0),
 
               // Teks
               Text(
                 'Setelah bersih dan rapi, kamu perlu mengaplikasikan base coat agar kuku kamu tetap sehat dan membuat nail art nantinya lebih tahan lama. Aplikasikan base coat secara merata, lalu diamkan sampai benar-benar kering. Menunggu base coat mengering memang membosankan Ladies, tapi tahap ini sangat penting agar nail art kamu hasilnya memuaskan.',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 14.0,
                 ),
               ),
-              SizedBox(height: 16.0), // Tambahkan jarak antara teks sebelumnya dan setelahnya
+              SizedBox(height: 16.0),
 
               Text(
                 'Mulailah Membuat Nail Art',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 8.0), // Beri sedikit jarak antara subjudul dan teks
+              SizedBox(height: 8.0),
 
               // Teks
               Text(
                 'Mulailah membuat nail art dengan referensi foto tentang tema nail art yang sudah kamu pilih. Tidak perlu terlalu fokus dengan referensi tersebut, kamu masih bisa kok bereksperimen dengan warna-warna favorit kamu asalkan sesuai dengan tema nail art yang kamu pilih. Agar hasilnya tidak mengecewakan, jangan lupa untuk menggunakan bantuan aplikator nail art.',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 14.0,
                 ),
               ),
-              SizedBox(height: 16.0), // Tambahkan jarak antara teks sebelumnya dan setelahnya
+              SizedBox(height: 16.0),
 
               Text(
                 'Aplikasikan Top Coat',
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 18.0,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(height: 8.0), // Beri sedikit jarak antara subjudul dan teks
+              SizedBox(height: 8.0),
 
               // Teks
               Text(
                 'Selain base coat, mengaplikasikan top coat juga sangat penting dan tidak boleh kamu lewatkan, Ladies. Top coat akan membantu nail art kamu lebih tahan lama dan memberikan efek kilau. Nah, jika ingin hasil nail art kamu tahan lebih lama dan berkilau, pastikan mengaplikasikan top coat sebagai finishing-nya ya.',
                 style: TextStyle(
-                  fontSize: 16.0,
+                  fontSize: 14.0,
                 ),
               ),
             ],
